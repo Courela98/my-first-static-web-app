e8d6bad22061b7af14b9086d2b433d26972cb74f
Rodrigo Courela <rcourela2@gmail.com>
2026-04-23 22:09:31 +0100

    Index

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Bem-vindo a Azure App Service - Node.js');
+  res.send('Bem-vindo a Azure App Service - Node.js | Após alteração');
 });
 
 const port = process.env.PORT || 3000;
